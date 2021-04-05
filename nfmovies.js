9a3069008453c757d248101deff4dba261b1de09
gitehabu <78459250+gitehabu@users.noreply.github.com>
2021-04-05 14:48:10 +0800

    Create nfmovies.js

---
+++ nfmovies.js
@@ -0,0 +1,2 @@
+var QC = document.querySelector("div.myui-ra-container.container")
+QC.innerHTML = '<a href="https://limbopro.xyz/usr/uploads/2020/10/2091577197.png"><img src="https://limbopro.xyz/usr/uploads/2020/10/2091577197.png" alt="Adsblock" title="CHINA" border="0"></a>';
