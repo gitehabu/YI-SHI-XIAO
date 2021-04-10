529588870df5144ac65b86fc55baf31156df88e5
gitehabu <78459250+gitehabu@users.noreply.github.com>
2021-04-10 17:01:23 +0800

    Create 10000.f

---
+++ 10000.f
@@ -0,0 +1,55 @@
+# umeng
+host-keyword, umeng, reject
+host-keyword, umsns, reject
+# 见智
+host, msg.umengcloud.com, reject
+host, eco.taobao.com, reject
+# 山丘阅读器
+host-suffix, uc.cn, reject
+host-suffix, wdsou.com, reject
+# 云听
+host, beacon-api.aliyuncs.com, reject
+host, oss-bj-radio-client.oss-cn-beijing.aliyuncs.com, reject
+host, adash.man.aliyuncs.com, reject
+host, httpdns.n.netease.com, reject
+host, n.sinaimg.cn, reject
+host, mlvbdc.live.qcloud.com, reject
+host, sdk.open.talk.igexin.com, reject
+host, ytapi.radio.cn, reject
+host-suffix, mob.com, reject
+host, redirect.networkbench.com, reject
+host, cnvod.cnr.cn, reject
+host, report.radio.cn, reject
+host-keyword, getui, reject
+host, ytmedia.radio.cn, reject
+host, inappcheck.itunes.apple.com, reject
+host, mpush-api.aliyun.com, reject
+# 文华财经随身行
+host, m-log.wenhua.com.cn, reject
+host, m-count.wenhua.com.cn, reject
+host, m-news-net.wenhua.com.cn, reject
+host, m-open.wenhua.com.cn, reject
+host, m-risk.wenhua.com.cn, reject
+host, m-adphone.wenhua.com.cn, reject
+# 推特视频下载https://www.downloadtwittervideo.com/zh-cn/
+host, rotumal.com, reject
+# 水印精灵
+host-suffix, pstatp.com, reject
+host-suffix, dobenge.cn, reject
+# Wind万得
+host-suffix, myqcloud.com, reject
+# 今日头条
+host, dm.toutiao.com, reject
+host, dm.bytedance.com, reject
+# 人人影视
+host, eleven.bayescom.cn, reject
+host, adash-emas.cn-hangzhou.aliyuncs.com, reject
+host, qcloud.rr.tv, reject
+host-suffix, pglstatp-toutiao.com, reject
+host-keyword, snssdk, reject
+host, dm-hl.toutiao.com, reject
+host-suffix, netease.im, reject
+host-keyword, fengkongcloud, reject
+host, qzs.qq.com, reject
+host-keyword, hpplay, reject
+host-keyword, wostore, reject
