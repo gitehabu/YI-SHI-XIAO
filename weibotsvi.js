1c71182c742a4faf0ee5b9ca8ea4e4442ecdb172
gitehabu <78459250+gitehabu@users.noreply.github.com>
2021-04-22 08:22:03 +0800

    Update weibotsvi.js

--- weibotsvi.js
+++ weibotsvi.js
@@ -182,7 +182,7 @@ function ParseWeibo(obj) {
                     }
                 }
             }
-            detail += '\n\n📌 点击跳转至全文及原微博。'
+            detail += '\n\n👠 点击跳转至全文及原微博。'
             if (releaseTime > $.update) $.notify(Title, subTitile, detail, open, showimg)
         //})
     }
